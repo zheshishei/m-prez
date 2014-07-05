f96bb15d2bca90fa84cfee7d5b62b6e88eb301ab
zheshishei <asun0927@gmail.com>
2014-07-05 00:07:16 -0400

    initial server

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+
 var app = express();
+var server = http.createServer(app);
+var io = socketio.listen(server);
 
 
+// Set the view directory to /views
+app.set("views", __dirname + "/views");
+
+// Let's use the Jade templating language
+app.set("view engine", "jade");
+
+app.get("/controller", function(request, response){
+	response.render("controller");
+});
+
+app.get("*", function(request, response){
+	response.render("index");
+});
+
+server.listen(8080);
+console.log("Server running at http://localhost:8080/");
