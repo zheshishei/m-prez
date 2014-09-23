2e88e573335418b7c01685236bc1facda24d3f9a
Alan Sun <alan@Loxxy.local>
2014-09-22 22:57:16 -0400

    server finally serves js and css

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var socketio = require('socket.io');
+var path = require('path');
 
 var app = require('express')();
 var server = require('http').Server(app);
@@ -12,12 +13,15 @@ app.set("views", __dirname + "/views");
 // Set the template engine to Jade
 app.set("view engine", "jade");
 
+//set the base directory to public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/controller", function(request, response){
 	response.render("controller");
 });
 
 app.get("*", function(request, response){
-	response.render("index");
+	response.render("index", {layout:false});
 });
 
 server.listen(8080);
@@ -29,4 +33,4 @@ process.on( 'SIGINT', function() {
   // some other closing procedures go here
   process.exit( );
 	console.log("Server has been shut down.");
-})
+});
